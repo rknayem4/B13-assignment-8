dfe02390d44205c69962aeb7dcaf8cd056065159
rknayem4 <rknayem10@gmail.com>
2026-04-30 20:06:02 +0600

    added marque and top items

---
+++ src/Components/TopCollection.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import TilesCard from "./TilesCard";
+import Link from "next/link";
+
+const TopCollection = async () => {
+  const res = await fetch("https://tiles-api-server-yz1c.vercel.app/data.json");
+  const tilesData = await res.json();
+  const top = tilesData.slice(6, 12);
+  return (
+    <div className="text-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-5 p-3">
+        {top.map((data) => (
+          <TilesCard key={data.id} data={data}></TilesCard>
+        ))}
+      </div>
+      <Link className="btn w-[30vw] my-3 bg-[#2A2B2A] text-white rounded-full " href="/all-tiles">
+        Explore More
+      </Link>
+    </div>
+  );
+};
+
+export default TopCollection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HeroBanner from "@/Components/Banner";
 import Marque from "@/Components/Marquee";
+import TopCollection from "@/Components/TopCollection";
 import Image from "next/image";
 
 export default function Home() {
   return <div className="">
     <HeroBanner></HeroBanner>
     <Marque></Marque>
+    <TopCollection></TopCollection>
   </div>;
 }
