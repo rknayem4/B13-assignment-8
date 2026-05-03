e8c70677b98bd4248c50df905ab304d8b50cd500
rknayem4 <rknayem10@gmail.com>
2026-05-03 18:25:04 +0600

    added details page tag & creator

--- src/app/tile/[id]/page.jsx
+++ src/app/tile/[id]/page.jsx
@@ -19,14 +19,29 @@ const TileDetailsPage = async ({ params }) => {
     category,
     inStock,
   } = data;
-  // console.log(data);
+  let creator;
+  let tag;
+
+  if (category == "ceramic") {
+    creator = "Rahul ";
+    tag = "Blue";
+  } else if (category == "porcelain") {
+    creator = "Rony";
+    tag = "Minimalist";
+  } else if (category == "glass") {
+    creator = "Rajib Gandi";
+    tag = "Blue";
+  } else {
+    creator = "Rayhan Khan";
+    tag = "Minimalist";
+  }
 
   return (
-    <div className="  container mx-auto p-5 flex max-md:flex-col gap-3 justify-between ">
+    <div className="  container mx-auto p-5 flex max-md:flex-col gap-3 justify-between border border-gray-100 shadow rounded-2xl m-5 ">
       <div className="flex-1">
         <Image src={image} alt={tiles} width={700} height={600}></Image>
       </div>
-      <div className="text-left flex-1 max-md:text-center space-y-3">
+      <div className="text-left flex-1 max-md:text-center relative space-y-3">
         <h2 className="text-2xl md:text-4xl font-bold">{title}</h2>
         <p>{description}</p>
         <h5 className="text-3xl font-bold my-6">
@@ -36,7 +51,10 @@ const TileDetailsPage = async ({ params }) => {
         <div className="divider"></div>
         <p> Size : {dimensions}</p>
         <p>Material : {material}</p>
-
+        <p>Creator : {creator}</p>
+        <div className="badge badge-soft badge-primary absolute top-0 right-0 m-6">
+          {tag}
+        </div>
         <span>
           {inStock ? (
             <div className="badge badge-outline badge-success">In Stock</div>
